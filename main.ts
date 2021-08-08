52c42857b279bc764f49baddead81c6255d4045e
Robert Vejvoda <robert.vejvoda@hotmail.com>
2021-08-08 19:00:52 +0200

    Condition switch

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
-    if (input.lightLevel() <= 120) {
-        RingbitCar.back()
+    if (input.lightLevel() >= 80) {
+        RingbitCar.forward()
     } else {
         RingbitCar.turnleft()
     }
